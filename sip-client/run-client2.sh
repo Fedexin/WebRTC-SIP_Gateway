880965b4cc20c63210cb02ca5aa037740ac39648
Fedexin <fedexin.03@gmail.com>
2025-11-09 12:26:38 +0100

    changes

--- sip-client/run-client2.sh
+++ sip-client/run-client2.sh
@@ -12,6 +12,15 @@ CP=$(cat /tmp/mjsip-examples-cp.txt):target/mjsip-examples-2.0.5.jar
 
 mkdir -p "/Users/fede/Documents/Scuola/UniPr/Tirocinio/WebRTC-SIP_Gateway/sip-client/log"
 
-exec java -Dtinylog.configuration="$TINYLOG_CFG" -cp "$CP" org.mjsip.examples.UserAgentCli \
-  -f "$CLIENT_CFG"
+# Se passi un argomento, viene usato come URI per chiamare automaticamente
+# Esempio: ./run-client2.sh "sip:client1@192.168.1.212"
+CALL_TO="${1:-}"
+
+if [ -n "$CALL_TO" ]; then
+  exec java -Dtinylog.configuration="$TINYLOG_CFG" -cp "$CP" org.mjsip.examples.UserAgentCli \
+    -f "$CLIENT_CFG" --call-to "$CALL_TO"
+else
+  exec java -Dtinylog.configuration="$TINYLOG_CFG" -cp "$CP" org.mjsip.examples.UserAgentCli \
+    -f "$CLIENT_CFG"
+fi
 
