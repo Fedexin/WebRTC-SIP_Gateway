bd8f26a558d5b703d731acec70ebf923850f1f4e
Federico Ciarniello <fedexin.03@gmail.com>
2025-11-17 16:23:41 +0100

    updates

--- sip-client/run-client1.sh
+++ sip-client/run-client1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-CLIENT_CFG="/Users/fede/Documents/Scuola/UniPr/Tirocinio/WebRTC-SIP_Gateway/sip-client/client1.cfg"
-TINYLOG_CFG="/Users/fede/Documents/Scuola/UniPr/Tirocinio/WebRTC-SIP_Gateway/sip-client/tinylog.properties"
+CLIENT_CFG="$HOME/tmp/WebRTC-SIP_Gateway/sip-client/client1.cfg"
+TINYLOG_CFG="$HOME/tmp/WebRTC-SIP_Gateway/sip-client/tinylog.properties"
 
-cd "/Users/fede/Documents/Scuola/UniPr/Tirocinio/WebRTC-SIP_Gateway/mjSIP-2.0.5/mjsip-examples"
+cd "$HOME/tmp/WebRTC-SIP_Gateway/mjSIP-2.0.5/mjsip-examples"
 
 # Classpath runtime per gli examples (include tinylog runtime già dichiarati)
 mvn -q -DincludeScope=runtime dependency:build-classpath -Dmdep.outputFile=/tmp/mjsip-examples-cp.txt
 CP=$(cat /tmp/mjsip-examples-cp.txt):target/mjsip-examples-2.0.5.jar
 
-mkdir -p "/Users/fede/Documents/Scuola/UniPr/Tirocinio/WebRTC-SIP_Gateway/sip-client/log"
+mkdir -p "$HOME/tmp/WebRTC-SIP_Gateway/sip-client/log"
 
 # Se passi un argomento, viene usato come URI per chiamare automaticamente
 # Esempio: ./run-client1.sh "sip:client2@192.168.1.212"
